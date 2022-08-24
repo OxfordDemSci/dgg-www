9335f16553295bbf1e7c7a77c141533f2f394dc4
vallerrr <36286934+vallerrr@users.noreply.github.com>
2022-08-24 11:31:28 +0100

    initial commit of the dockerised postgres

---
+++ sql/dgg_initial.sql
@@ -0,0 +1,16 @@
+BEGIN;
+CREATE TABLE dgg(
+		date            numeric (6) not null,
+	    country         varchar(80) not null,
+	    ISO3Code         char(3) not null,
+	    ISO2Code         char(2) not null,
+	    Ground_Truth_Internet_GG  numeric  ,
+	    Internet_Online_model_prediction numeric  ,
+	    Internet_Online_Offline_model_prediction numeric  ,
+		Internet_Offline_model_prediction numeric ,
+	    Ground_Truth_Mobile_GG numeric ,
+	    Mobile_Online_model_prediction numeric,
+	    Mobile_Online_Offline_model_prediction numeric  ,
+	    Mobile_Offline_model_prediction numeric
+);
+COMMIT;
