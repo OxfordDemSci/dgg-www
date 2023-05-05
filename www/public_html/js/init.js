1fc8a78b50e36ff97f03b773234b677e193cd358
Maksym Bondarenko <dm@gisrede.com>
2023-05-05 16:36:14 +0100

    Update init.js
    changed downloading menu to download last 6 month.

--- www/public_html/js/init.js
+++ www/public_html/js/init.js
@@ -184,8 +184,41 @@ export function loadDatesToMenu(firstMonth, firstYear, lastMonth, lastYear, mont
 
 }
 
-export function loadDatesToDownloadMenu(firstMonth, firstYear, lastMonth, lastYear, monthsToDisable) {
+export function getLastSixMonthDate(firstMonth, firstYear, lastMonth, lastYear, monthsToDisable) {
+   // getting the date of last 6 month from lastMonth and lastYear
+   // cheecking if this date is not in monthsToDisable
+   // if it is in monthsToDisable then taken increament and looking for next month etc
+   
+    let icount=6;
+    let max_icount=18;
+    let lastYM;
+    let six_month_lastYM;
+    let six_month_lastYM_Date;
+   
+   do {
+        lastYM = new Date(lastYear+ '-' + lastMonth);
+        // getting last "icount" month from "lastYM"
+        six_month_lastYM = lastYM.setMonth(lastYM.getMonth() - icount);
+        six_month_lastYM_Date = moment(six_month_lastYM).format('YYYY-MM');
+        icount++;
+        
+        // avoiding infiniting loop if last 6 month in  monthsToDisable array 
+        // so the check will be don maximum max_icount times
+        // if did not pick last 6 month, the firstYear-firstMonth will be return
+        if (max_icount < icount) {
+            six_month_lastYM_Date=firstYear+ '-' + firstMonth;
+            break;
+        }        
+    }
+   while ($.inArray(six_month_lastYM_Date, monthsToDisable) > 0);
+    
+   return six_month_lastYM_Date;
+}
 
+
+export function loadDatesToDownloadMenu(firstMonth, firstYear, lastMonth, lastYear, monthsToDisable) {
+  
+    var LastSixMonthDate=getLastSixMonthDate(firstMonth, firstYear, lastMonth, lastYear, monthsToDisable).split("-");
 
     $("#datepicker_start_date").datepicker({
         format: "yyyy-mm",
@@ -203,7 +236,7 @@ export function loadDatesToDownloadMenu(firstMonth, firstYear, lastMonth, lastYe
         }
     });
 
-    $('#datepicker_start_date').datepicker('setDate', firstYear + '-' + firstMonth);
+    $('#datepicker_start_date').datepicker('setDate', LastSixMonthDate[0] + '-' + LastSixMonthDate[1]);
     
     
     $("#datepicker_end_date").datepicker({
