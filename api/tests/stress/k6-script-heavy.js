f1617b8b38307756dba6155c1cc05bc8bb176e3e
GISRedeDev <dm@gisrede.com>
2024-11-16 14:22:53 +0000

    Alter stress tests

--- api/tests/stress/k6-script-heavy.js
+++ api/tests/stress/k6-script-heavy.js
@@ -13,7 +13,7 @@ export const options = {
         { duration: '5s', target: 0 },  // ramp down to 0 users over 5 seconds
     ],
     thresholds: {
-        http_req_duration: ['p(95)<3000'], // 95% of requests must complete below 2 seconds
+        http_req_duration: ['p(95)<20000'], // 95% of requests must complete below 2 seconds
         http_req_failed: ['rate<0.05'], // less than 1% of requests should fail
         http_reqs: ['rate>0.5'],            // at least 10 requests per second
         iteration_duration: ['p(95)<5000'], // 95% of iterations should complete within 2 seconds
