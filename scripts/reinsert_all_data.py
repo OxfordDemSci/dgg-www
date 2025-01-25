f1a2d2c0b3e7f41a3c4d148c856b4f814c84346c
GISRedeDev <dm@gisrede.com>
2025-01-25 16:23:44 +0000

    Tidy up

--- scripts/reinsert_all_data.py
+++ scripts/reinsert_all_data.py
@@ -68,26 +68,12 @@ def get_country_name(row):
         return row['gid_0']
 
 
-# @profile
-# def upload_csv_to_indicators(csv_path, table_model, session, chunk_size=1000):
-#     df = pd.read_csv(csv_path)
-#     df['date'] = pd.to_datetime(df['date'], format="%Y-%m").dt.strftime('%Y-%m')
-#     df['country'] = df.apply(get_country_name, axis=1)
-#     df = df.replace({np.nan: None, 'NaN': None, 'nan': None, 'null': None, 'NULL': None, 'None': None})
-#     for i in range(0, len(df), chunk_size):
-#         data_chunk = df.iloc[i:i+chunk_size].to_dict(orient='records')
-#         session.bulk_insert_mappings(table_model, data_chunk)
-#         session.commit()
-
-@profile
 def upload_csv_to_indicators(csv_path, table_model, session, chunk_size=1000, max_workers=4):
     df = pd.read_csv(csv_path)
     df['date'] = pd.to_datetime(df['date'], format="%Y-%m").dt.strftime('%Y-%m')
     df['country'] = df.apply(get_country_name, axis=1)
-    df = df.replace({np.nan: None, 'NaN': None, 'nan': None, 'null': None, 'NULL': None, 'None': None})
 
     def upload_chunk(data_chunk):
-        print("DONE A CHUNK")
         local_session = sessionmaker(bind=session.get_bind())()
         try:
             local_session.bulk_insert_mappings(table_model, data_chunk)
